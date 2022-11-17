410390b3b49a505c6f2eda261e1e5a17a1ff7f8e
IndianBartonka <68228599+IndianBartonka@users.noreply.github.com>
2022-11-17 19:16:53 +0100

    Add files via upload

--- src/main/java/me/indian/pl/Formater.java
+++ src/main/java/me/indian/pl/Formater.java
@@ -42,30 +42,19 @@ public class Formater extends PluginBase implements Listener {
         Config conf = this.getConfig();
 
 
-        String windows = conf.getString("Windows");
-        String android = conf.getString("Android");
-        String ios = conf.getString("Ios");
-        String mac = conf.getString("Mac");
-        String fire = conf.getString("Fire");
-        String gearvr = conf.getString("Gearvr");
-        String hololens = conf.getString("Hololens");
-        String dedicated = conf.getString("Hedicated");
-        String tvos = conf.getString("TvOs");
-        String playstation = conf.getString("PlayStation");
-        String nx = conf.getString("Nx");
-        String xbox = conf.getString("Xbox");
-        String unknow = conf.getString("Unknow");
 
-        String ping = "";
-        if (p.getPing() >= 1) {
-            ping = "§a" + p.getPing();
-        }
-        if (p.getPing() >= 75) {
-            ping = "§e" + p.getPing();
-        }
-        if (p.getPing() >= 100) {
-            ping = "§c" + p.getPing();
-        }
+        String cenzura = this.getConfig().getString("censorship");
+
+        for (String czarnalista : this.getConfig().getStringList("BlackWords")) {
+            if (e.getMessage().toLowerCase().contains(czarnalista.toLowerCase())) {
+                if (e.getMessage().toLowerCase().contains("Huje22".toLowerCase())) {
+                    return;
+                }
+            }
+            if(!(p.isOp())) {
+                msg = e.getMessage().toLowerCase().replace(czarnalista.toLowerCase(), cenzura);
+                e.setMessage(msg);
+            }
 
 
         LuckPerms luckPerms = LuckPermsProvider.get();
@@ -79,30 +68,19 @@ public class Formater extends PluginBase implements Listener {
                         .replace("<prefix>", prefix)
                         .replace("<suffix>", suffix)
                         .replace("<msg>", msg)
-                        .replace("<device>", p.getLoginChainData().getDeviceOS() + "")
-                        .replace("1", android)
-                        .replace("2", ios)
-                        .replace("3", mac)
-                        .replace("4", fire)
-                        .replace("5", gearvr)
-                        .replace("6", hololens)
-                        .replace("7", windows)
-                        .replace("8", windows)
-                        .replace("9", dedicated)
-                        .replace("10", tvos)
-                        .replace("11", playstation)
-                        .replace("12", nx)
-                        .replace("13", xbox)
-                        .replace("§7", "§7")
+                        .replace("<device>", getDevice(p, this))
                         .replace("<health>", p.getHealth() + "")
                         .replace("<model>", p.getLoginChainData().getDeviceModel() + "")
                         .replace("<version>", p.getLoginChainData().getGameVersion())
                         .replace("<language>", p.getLoginChainData().getLanguageCode())
-                        .replace("<ping>", ping)
-
-
+                        .replace("<ping>", getPing(p))
                 //message.format: "<prefix> <player> <suffix> >> <msg>
         );
+
+
+
+
+        }
     }
 
     public static String getPrefix(User user){
@@ -124,5 +102,61 @@ public class Formater extends PluginBase implements Listener {
         }
         return suffix;
     }
+
+
+    public static String getDevice(Player p , Formater plugin){
+        Config conf = plugin.getConfig();
+        String windows = conf.getString("Windows");
+        String android = conf.getString("Android");
+        String ios = conf.getString("Ios");
+        String mac = conf.getString("Mac");
+        String fire = conf.getString("Fire");
+        String gearvr = conf.getString("Gearvr");
+        String hololens = conf.getString("Hololens");
+        String dedicated = conf.getString("Hedicated");
+        String tvos = conf.getString("TvOs");
+        String playstation = conf.getString("PlayStation");
+        String nx = conf.getString("Nintendo");
+        String xbox = conf.getString("Xbox");
+        String unknow = conf.getString("Unknow");
+
+
+        String dev = "" + p.getLoginChainData().getDeviceOS();
+
+
+        String device = dev
+                .replace("1", android)
+                .replace("2", ios)
+                .replace("3", mac)
+                .replace("4", fire)
+                .replace("5", gearvr)
+                .replace("6", hololens)
+                .replace("7", windows)
+                .replace("8", windows)
+                .replace("9", dedicated)
+                .replace("10", tvos)
+                .replace("11", playstation)
+                .replace("12", nx)
+                .replace("13", xbox);
+
+        return device;
+    }
+
+    public static String getPing(Player p ){
+
+        String ping = "";
+        if (p.getPing() >= 1) {
+            ping = "§a" + p.getPing();
+        }
+        if (p.getPing() >= 75) {
+            ping = "§e" + p.getPing();
+        }
+        if (p.getPing() >= 100) {
+            ping = "§c" + p.getPing();
+        }
+        return ping;
+    }
+
+
 
 }
